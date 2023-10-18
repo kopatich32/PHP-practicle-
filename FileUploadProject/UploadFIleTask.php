3f907091d4d7219439f8d5eff9d12c73b26f0f6a
user.name <kopatich1994@yandex.ru>
2023-10-18 17:19:04 +0300

    .

--- FileUploadProject/UploadFIleTask.php
+++ FileUploadProject/UploadFIleTask.php
@@ -25,17 +25,19 @@ public function checkFolder($folder, $inputFIle){
 }
     public function UploadFile($folder, $inputFIle){
         $Full_year = ['Января' , 'Февраля' , 'Марта' , 'Апреля' , 'Мая' , 'Июня' , 'Июля' , 'Августа' , 'Сентября' , 'Октября' , 'Ноября' , 'Декабря'];
+        date_default_timezone_set('Europe/Moscow');
         $date = getdate()['mon'];
-        $currentHour = getdate()['hours'] +3;
+        $currentHour = getdate()['hours'];
         $translateMonth = $Full_year[$date -1] ;
-        $timeStamp = date("d $translateMonth Y, $currentHour:i:s");
-       $log_file =file_put_contents('Log_of_update', $timeStamp, FILE_APPEND);
+
+        $timeStamp = date("d $translateMonth Y, $currentHour:i:s") . ' File name: ' . $_FILES[$inputFIle]['name'];
+       $log_file =file_put_contents('Log_of_update.txt', $timeStamp . '\n', FILE_APPEND);
         $path_to =  $_FILES[$inputFIle]['name'];
             move_uploaded_file($_FILES[$inputFIle]['tmp_name'], $folder . '/'. $path_to);
         file_get_contents('Log_of_update');
         move_uploaded_file($log_file, $folder . '/'. $path_to);
 
-
+echo $timeStamp;
     }
 }
 
