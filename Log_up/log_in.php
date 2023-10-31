9725be29112a39922eb0b98e32bcf156210bd4db
kopatich32 <kopatich1994@yandex.ru>
2023-10-31 21:04:44 +0300

    .

--- Log_up/log_in.php
+++ Log_up/log_in.php
@@ -10,18 +10,42 @@
 </head>
 <body>
 <?php
-include '../OOPzoo/localhost/DBCrud.php';
-$usersdb = new DBCrud('users','localhost','root','', 'comment');
-if($_POST['send'])
+$status = false;
+if($_POST['send']){
+    $login = $_POST['login'];
+    $pass = $_POST['password'];
+    if(!empty($login) and !empty($pass)){
+        $db = @new mysqli('localhost','root','','comment');
+        if($db->connect_errno){
+            echo "error: " . $db->connect_errno . ' number: ' . $db->connect_error;
+        }else{
+            $query = $db->query("SELECT * FROM `login` WHERE `login` = '$login' AND `password` = '$pass'" );
+            $row = $query->fetch_assoc();
+            if(($row)){
+                $status = true;
+                echo 'access';
+                echo $row['email'];
+            }
+            if($row == null){
+                $status = false;
+                echo 'nope';
+            }
+        }
+
+    }
+    echo $_POST['login'];
+    echo $_POST['password'];
+}
+
 ?>
 
 
 <div class="no">no</div>
 <div class="yes">yes</div>
 <form method="POST">
-    <input type="text" placeholder="email" name="login">
+    <input type="text" placeholder="login" name="login">
     <input type="text" placeholder="password" name="password">
-    <input type="submit" value="send">
+    <input type="submit" name="send">
 
 </form>
 </body>
